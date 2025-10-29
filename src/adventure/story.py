5654ee7c84b326fd1725812b42a6cd2222c7f1ba
Yuechun Qin <Yqin2-cosc381@users.noreply.github.com>
2025-10-29 15:52:04 +0000

    use rich library to format and style text

--- src/adventure/story.py
+++ src/adventure/story.py
@@ -1,5 +1,17 @@
 from adventure.utils import read_events_from_file
+from rich.console import Console
+from rich.theme import Theme
 import random
+
+adventure_theme = Theme({
+    "head":"bold #407b7c",
+    "question": "bold magenta underline",
+    "left_choice": "cyan",
+    "right_choice": "yellow",
+    "exit_choice": "red",
+    "left": "green",
+    "right": "blue"
+})
 
 def step(choice: str, events):
     random_event = random.choice(events)
@@ -12,19 +24,20 @@ def step(choice: str, events):
         return "You stand still, unsure what to do. The forest swallows you."
 
 def left_path(event):
-    return "You walk left. " + event
+    return "[left]You walk left. " + event+"[/left]"
 
 def right_path(event):
-    return "You walk right. " + event
+    return "[right]You walk right. " + event+"[/right]"
 
 if __name__ == "__main__":
     events = read_events_from_file('events.txt')
-
-    print("You wake up in a dark forest. You can go left or right.")
+    console = Console(theme=adventure_theme)
+    console.print("[head]You wake up in a dark forest. You can go left or right.[/head]")
     while True:
-        choice = input("Which direction do you choose? (left/right/exit): ")
+        choice = console.input("[question]Which direction do you choose?[/question] ([left_choice]left[/left_choice]/[right_choice]right[/right_choice]/[exit_choice]exit[/exit_choice]): ")
+
         choice = choice.strip().lower()
         if choice == 'exit':
             break
         
-        print(step(choice, events))
+        console.print(step(choice, events))
